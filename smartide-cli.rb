fe21106ab1ba024d71ee35fca0a814977c3bf01b
JackyZhou <info@zhouwenyang.com>
2021-09-02 22:01:51 +0800

    fix download url

--- smartide-cli.rb
+++ smartide-cli.rb
@@ -4,7 +4,7 @@
 class SmartideCli < Formula
   desc "SmartIDE CLI tool makes it easy to run Your Development Environment"
   homepage "https://smartide.cn"
-  url "https://github.com/leansoftX/smartide-cli/releases/download/v1.0/smartide-osx-x64.zip"
+  url "https://github.com/leansoftX/smartide-cli/releases/download/v0.1.001/smartide-osx-x64.zip"
   sha256 "276892af109b1f93e30f41bfb2c59626ceeebea8a57b64200aa08a388602083b"
   license "Apache-2.0"
 
